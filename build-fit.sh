f28a67444b127760c56f8a705b23f1831d62f6d3
Robbie VanVossen <robert.vanvossen@dornerworks.com>
2015-07-20 14:15:49 -0400

    The buid-fit script now goes into the correct directory before running mkimage

--- build-fit.sh
+++ build-fit.sh
@@ -6,4 +6,5 @@ cp xen/xen/xen FIT/xen
 dtc -I dts -O dtb -o FIT/sun7i-a20-cubietruck.dtb FIT/sun7i-a20-cubietruck.dts
 dtc -I dts -O dtb -o FIT/xen.dtb FIT/xen.dts
 
+cd FIT/
 mkimage -f cubietruck-xen.its cubietruck-xen.itb
