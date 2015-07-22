a5d505a1580bb83d4bc4e2f1a825deb94515b576
Robbie VanVossen <robert.vanvossen@dornerworks.com>
2015-07-22 09:21:54 -0400

    Changed the FIT Image Source File configurations a bit

--- build-fit.sh
+++ build-fit.sh
@@ -5,6 +5,7 @@ cp xen/xen/xen FIT/xen
 
 dtc -I dts -O dtb -o FIT/sun7i-a20-cubietruck.dtb FIT/sun7i-a20-cubietruck.dts
 dtc -I dts -O dtb -o FIT/xen.dtb FIT/xen.dts
+dtc -I dts -O dtb -o FIT/xen2.dtb FIT/xen2.dts
 
 cd FIT/
 mkimage -f cubietruck-xen.its cubietruck-xen.itb
