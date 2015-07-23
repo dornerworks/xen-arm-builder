0424cda97a60cc0aa2dac1576fb70887d3fde486
Robbie VanVossen <robert.vanvossen@dornerworks.com>
2015-07-23 15:46:02 -0400

    No longer installing the u-boot-tools and using the mkimage utility built with our u-boot source

--- build-fit.sh
+++ build-fit.sh
@@ -8,4 +8,4 @@ dtc -I dts -O dtb -o FIT/xen.dtb FIT/xen.dts
 dtc -I dts -O dtb -o FIT/xen2.dtb FIT/xen2.dts
 
 cd FIT/
-mkimage -f cubietruck-xen.its cubietruck-xen.itb
+../u-boot/build-cubietruck/tools/mkimage -f cubietruck-xen.its cubietruck-xen.itb
